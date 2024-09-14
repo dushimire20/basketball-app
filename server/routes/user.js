6bcb3a12ef6bae7adfc411e36235e7ac540265b9
dushimire20 <odushimire922@daviscollege.com>
2024-09-14 18:16:29 +0200

    updates

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 import { User } from "../models/user.js";
 const router = express.Router();
 import jwt from 'jsonwebtoken';
+import nodemailer from "nodemailer";
 
 
 router.post('/signup', async (req, res) => {
@@ -45,5 +46,43 @@ router.post('/login', async (req, res) => {
     
     
 });
+
+router.post('/forgotPassword', async (req, res) => {
+    const {email} = req.body;
+
+    try {
+        const user = await User.findOne({email})
+        if(!user) {
+            return res.status(400).json({message: "User not found"})
+            }
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: 'youremail@gmail.com',
+                  pass: 'yourpassword'
+                }
+              });
+              
+              var mailOptions = {
+                from: 'youremail@gmail.com',
+                to: 'myfriend@yahoo.com',
+                subject: 'Sending Email using Node.js',
+                text: 'That was easy!'
+              };
+              
+              transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('Email sent: ' + info.response);
+                }
+              });
+
+        
+    } catch (error) {
+        console.log(error)        
+    }
+
+})
 
 export {router as UserRouter}
